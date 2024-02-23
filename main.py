689dbb45897d636c91a5da26291cacde88b21190
Peerasit <peerasitearth@outlook.com>
2024-02-23 22:49:34 +0700

    Add Comment and Author

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import numpy as np
+'''
+จัดทำโดย
+    นาย กิตติกานต์ มากผล 6410450087
+    นาย นิสิต นะมิตร 6410451148
+    นาย พีรสิษฐ์ พลอยอร่าม 6410451237
+    นาย ศิวกร ภาสว่าง 6410451423
+    ข้อ 1
+'''
 
 index = 0 # Index of Token
 
@@ -7,33 +14,34 @@ def match(char):
     global index
 
     if token[index] == char:
-        index += 1
+        index += 1                      # Move Pointer
         return "Match! Advance Input"
     else:
         return f"Mismatch: {char} is not{token[index]} ! backtrack..."
 
+
 buffer = [] # Buffer of input gramma
 couter = 0  # Couter of gramma
+
 while True:
     gramma = input("Input Gramma: ")
     if (gramma == 'exit'):
         break
-    buffer.append(gramma)
-    couter += 1
-    # a,b = gramma.split('=',1)
+    buffer.append(gramma)       # Add to Buffer
+    couter += 1                 # Count
 
-main_gramma = [couter]
+main_gramma = [couter]          # Init Main Buffer Gramma
 token = input("Input Token: ")
 
 for i in range(couter):
-    a,b = buffer[i].split('=',1)
+    a,b = buffer[i].split('=',1)        # Split Header and Gramma e -> t | t+e
     main_gramma.append(a)
 
-    test = b.split("|")
-    main_gramma.append(test)
-del main_gramma[0]
+    gramma_split = b.split("|")
+    main_gramma.append(gramma_split)    # t | t+e
+del main_gramma[0]                      # Delete Counter
 
-print(f"\nGramma is:\n------------------\n")
+print(f"\nGramma is:\n------------------\n")    # Print Gramma
 for i in range(0,len(main_gramma),2):
     print(f"{main_gramma[i]} -> ",end="")
     for j in main_gramma[i+1]:
@@ -41,20 +49,20 @@ for i in range(0,len(main_gramma),2):
     print('\n')
 
 print("------------------")
-#   ======================
-# Main
 
+#   ======================
+#   Main
 print(f"\nToken Stream is: {token}\n")
 for round in range(len(token)):
     print(f"------------------------------------")
-    for index_gramma in range(1,len(main_gramma),2):
-        for j in main_gramma[index_gramma]:
-            for k in j:
-                if index >= len(token):
+    for index_gramma in range(1,len(main_gramma),2):    # Index Loc Gramma
+        for j in main_gramma[index_gramma]:             # Gramma
+            for k in j:                                 # Each Gramma
+                if index >= len(token):                 # If Accept
                     print("End of input, accept")
                     exit()
-                print(f"Gramma[{k}] Token is: {token[index-1]} Index is: [{index}]")
-                print(f"Status: {match(k)}")
-                print('\n')
 
+                print(f"Gramma[{k}] Token is: {token[index-1]} Index is: [{index}]")
+                print(f"Status: {match(k)}\n")
+                                                        # Reject
 print("End of input, reject")
